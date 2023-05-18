7e9a49e405db9f0fa6183069581cd1b4654faba8
EfrosEugeniu-md <efrosevgenij@gmail.com>
2023-05-18 10:46:06 +0300

    init

---
+++ src/main/java/com/example/worktable/second/WorktableSecondApplication.java
@@ -0,0 +1,13 @@
+package com.example.worktable.second;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorktableSecondApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorktableSecondApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/worktable/second/WorktableSecondApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.worktable.second;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorktableSecondApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
